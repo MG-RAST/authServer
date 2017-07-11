c37f1cc4ec89e79a6b447ded405e5a32365ffe49
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2017-07-11 15:44:26 -0500

    support mysql, not working yet

--- dbsetup.sql
+++ dbsetup.sql
@@ -1,6 +1,6 @@
-CREATE TABLE user (login, password, cookie, name, email, confirmed BOOLEAN, admin BOOLEAN);
-CREATE TABLE apps (application, url, secret);
-CREATE TABLE accepts (login, application, token);
-CREATE TABLE tokens (token, login, created);
-CREATE TABLE scope (name, user);
-CREATE TABLE rights (type, item, edit BOOLEAN, view BOOLEAN, scope, owner BOOLEAN);
+CREATE TABLE user (login text NOT NULL UNIQUE, password text NOT NULL, cookie text, name text, email text, confirmed BOOLEAN, admin BOOLEAN);
+CREATE TABLE apps (application text NOT NULL, url text NOT NULL, secret text NOT NULL);
+CREATE TABLE accepts (login text NOT NULL UNIQUE, application text NOT NULL, token text NOT NULL);
+CREATE TABLE tokens (token text NOT NULL UNIQUE, login text NOT NULL, created text NOT NULL);
+CREATE TABLE scope (name text NOT NULL UNIQUE, user text NOT NULL);
+CREATE TABLE rights (type text NOT NULL UNIQUE, item text, edit BOOLEAN, view BOOLEAN, scope text, owner BOOLEAN);
