107407de77a059235200c9ab5f90926908c3ff60
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2017-07-12 14:01:32 -0500

    mysql tables

--- dbsetup.sql
+++ dbsetup.sql
@@ -1,6 +1,6 @@
-CREATE TABLE user (login text NOT NULL UNIQUE, password text NOT NULL, cookie text, name text, email text, confirmed BOOLEAN, admin BOOLEAN);
-CREATE TABLE apps (application text NOT NULL, url text NOT NULL, secret text NOT NULL);
-CREATE TABLE accepts (login text NOT NULL UNIQUE, application text NOT NULL, token text NOT NULL);
-CREATE TABLE tokens (token text NOT NULL UNIQUE, login text NOT NULL, created text NOT NULL);
-CREATE TABLE scope (name text NOT NULL UNIQUE, user text NOT NULL);
-CREATE TABLE rights (type text NOT NULL UNIQUE, item text, edit BOOLEAN, view BOOLEAN, scope text, owner BOOLEAN);
+CREATE TABLE user (login CHAR(20) PRIMARY KEY, password text NOT NULL, cookie text, name text, email text, confirmed BOOLEAN, admin BOOLEAN);
+CREATE TABLE apps (application CHAR(50) PRIMARY KEY, url text NOT NULL, secret text NOT NULL);
+CREATE TABLE accepts (login CHAR(20) PRIMARY KEY, application text NOT NULL, token text NOT NULL);
+CREATE TABLE tokens (token CHAR(100) PRIMARY KEY, login text NOT NULL, created text NOT NULL);
+CREATE TABLE scope (name CHAR(20) PRIMARY KEY, user text NOT NULL);
+CREATE TABLE rights (type CHAR(20) PRIMARY KEY, item text, edit BOOLEAN, view BOOLEAN, scope text, owner BOOLEAN);
