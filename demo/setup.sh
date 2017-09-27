4117f4710ed83ff05d806c092e654f8336d2f6c4
Andreas Wilke <wilke@mcs.anl.gov>
2017-09-27 09:49:47 -0500

    Demo setup

---
+++ demo/setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+result=1
+
+while [ $result -ne 0 ]
+do   
+ # mysql -u authService --password=authServicePassword --host db -T -D DemoAppUsers  < /tmp/dbsetup.demo.mysql
+ sleep 5
+ echo Initializing database `date`
+ mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD --host $MYSQL_HOST -T -D $MYSQL_DATABASE  < /tmp/dbsetup.demo.mysql
+ result=$?
+done
+
+httpd-foreground
